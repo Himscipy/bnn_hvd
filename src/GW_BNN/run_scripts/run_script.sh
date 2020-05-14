d698e884876820ca8195b3f47ed58a26226249f4
hsharma <himanshu.sharma@anl.gov>
2020-05-14 03:11:48 +0000

    Updated the .gitignore

--- src/GW_BNN/run_scripts/run_script.sh
+++ src/GW_BNN/run_scripts/run_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #COBALT -t 1:00:00
-#COBALT -n 1
-#COBALT -A mmaADSP
+#COBALT -n 4
+#COBALT -A datascience
 #COBALT -q debug-flat-quad 
 
 
@@ -10,15 +10,15 @@ export PYTHONPATH=$HOME/.local/lib/python3.6/site-packages:$PYTHONPATH
 
 
 PPN=1
-export NUM_THREADS=128
+NUM_THREADS=128
 
 aprun -n $((${COBALT_PARTSIZE} * ${PPN})) -N ${PPN} -cc depth -d ${NUM_THREADS} -j 2 -e OMP_NUM_THREADS=${NUM_THREADS} \
 python /projects/datascience/hsharma/bnn_hvd/src/GW_BNN/run_spin_omega_train.py \
  --fixed_var=0.0001 \
+ --num_iter=200 \
  --num_mc=100 \
- --alpha_KL=0.1 \
+ --alpha_KL=1 \
  --hvd=True \
  --pout=True \
  --Datasave_path='/projects/datascience/hsharma/data_store' \
- --TestIter=200 \
  --PrintStep=200 \
