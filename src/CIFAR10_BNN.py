388a778f46b44b0b104408f83a9992d65e394c54
Himanshu Sharma <himanshu.sharma@anl.gov>
2020-05-16 04:35:45 -0500

    Updated the CIFAR10 code

---
+++ src/CIFAR10_BNN.py
@@ -0,0 +1,389 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 24 15:37:45 2019
+
+@author: himanshu.sharma
+"""
+import os
+import cProfile
+import errno
+import tensorflow as tf
+import horovod.tensorflow as hvd
+import numpy as np
+import time
+from tensorflow import keras
+import pickle
+import tensorflow_probability as tfp
+import sys
+from absl import flags
+from utils import Pre_Post_Process,Model_CNN_BNN,Data_API
+
+try:
+  import seaborn as sns 
+  HAS_SEABORN = True
+except ImportError:
+  HAS_SEABORN = False
+
+
+tfp_layers = tfp.layers
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+tfd = tfp.distributions
+
+IMAGE_SHAPE = [32, 32, 3]
+
+flags.DEFINE_float("learning_rate",default=0.0001,help="Initial learning rate.")
+flags.DEFINE_integer("epochs", default= 700,help="Number of epochs to train for.")
+flags.DEFINE_integer("batch_size", default=128, help="Batch size. #16,32,128")
+flags.DEFINE_string("data_dir",default=os.path.join('./results', "bayesian_neural_network/data/"), help="Directory where data is stored (if using real data).")
+flags.DEFINE_string("model_dir",default=os.path.join('./results',"bayesian_neural_network/"),help="Directory to put the model's fit.")
+flags.DEFINE_integer("num_monte_carlo",default=80,help="Network draws to compute predictive probabilities.")
+flags.DEFINE_integer("print_step", default=10,help=" Frequency to perform printing")
+flags.DEFINE_integer("iterations", default=100,help=" Number of iterations for debugging purpose only")
+flags.DEFINE_string("DATA_PATH",default="/projects/datascience/hsharma/bnn_horovod/TFP_CIFAR10/RunScript/cifar-10-batches-py",help="Path of the data directory")
+flags.DEFINE_boolean("USE_EPOCH",default="False",help="If set true use the specified epochs to")
+flags.DEFINE_integer('num_intra', default=128,help='num_intra')
+flags.DEFINE_integer('num_inter', default=1,help='num_inter')
+flags.DEFINE_integer('kmp_blocktime', default=0,help='KMP BLOCKTIME')
+flags.DEFINE_string('kmp_affinity', default='granularity=fine,verbose,compact,1,0',help='KMP AFFINITY')
+flags.DEFINE_bool('verbose',default=False,help='Verbosre to print extra details about the model')
+flags.DEFINE_bool('kl_regularizer',default=True,help='Verbose to print extra details about the model')
+flags.DEFINE_float('Alpha',default=1.0,help='Use the scaling for the KL divergence term')
+flags.DEFINE_integer('eval_freq',default=100,help='Use the eval frequency to do the MC iteration')
+flags.DEFINE_bool('Alpha_State',default=False,help='Set Alpha term on/off')
+flags.DEFINE_bool('Restart',default=False,help='Set Alpha term on/off')
+flags.DEFINE_string('DATA_NAME',default='CIFAR-10',help='Set the data used for the study (Available: CIFAR-10,CIFAR-100)')
+
+
+FLAGS = flags.FLAGS
+
+
+
+
+
+def main(argv):
+    tf.reset_default_graph()
+    hvd.init()
+    
+    DataObj = Data_API(FLAGS)
+    
+    PrePostObj = Pre_Post_Process(FLAGS)
+    
+    config = PrePostObj.create_config_proto()
+
+    Seed_set = PrePostObj.Setup_Seed(hvd.rank())
+
+
+    class Dummy():
+        pass
+
+    
+    del argv  # unused
+
+    if hvd.rank() == 0:
+        print("Total Number of workers",hvd.size())
+        
+        if tf.io.gfile.exists(FLAGS.model_dir):
+            tf.compat.v1.logging.warning(
+                "Exists: log directory at {} for restart".format(FLAGS.model_dir))
+        else:
+            tf.compat.v1.logging.warning(
+                "Warning: Creating log directory at {}".format(FLAGS.model_dir))
+            #tf.io.gfile.rmtree(FLAGS.model_dir)
+            tf.io.gfile.makedirs(FLAGS.model_dir)
+            tf.io.gfile.makedirs(FLAGS.data_dir)
+    else:
+        None
+
+    (x_train, y_train), (x_test, y_test) = DataObj.load_data(hvd.rank())
+
+    K = 10
+
+    ModelObj = Model_CNN_BNN(*IMAGE_SHAPE,K)
+
+    if FLAGS.verbose:
+         if hvd.rank() == 0:
+             print ('Label size',y_train.shape)
+             print ('Traint data size',x_train.shape)
+    
+    with tf.name_scope('input'):
+        images = tf.placeholder(tf.float32, [None, *IMAGE_SHAPE], name='image')
+        labels = tf.placeholder(tf.int32, [None], name='label')
+    
+
+    if FLAGS.USE_EPOCH:
+        training_steps = int(round(FLAGS.epochs * (len(x_train) / FLAGS.batch_size)))
+    else:
+        training_steps = FLAGS.iterations # 100 # Debugging.... only.!
+
+
+
+    if hvd.rank() == 0:
+        with open( os.path.join(FLAGS.data_dir + "Parsed_Arg"+".log"), 'w') as _out:
+            _out.write(str(FLAGS.flag_values_dict()))
+            _out.flush()
+            _out.close()
+        print("Training iterations",training_steps)
+        print ('*'*60)
+        print (FLAGS.flag_values_dict(),flush=True)
+        print ('*'*60)
+
+    
+    model = ModelObj.CIFAR10_BNN_model()
+    logits = model(images)
+
+    labels_distribution = tfd.Categorical(logits=logits)
+
+    # Perform KL annealing. The optimal number of annealing steps
+    # depends on the dataset and architecture.
+    
+    t = tf.Variable(0.0,use_resource=True)
+    
+    kl_regularizer = 1.0
+    
+    # Compute the -ELBO as the loss. The kl term is annealed from 0 to 1 over
+    # the epochs specified by the kl_annealing flag.
+    log_likelihood = labels_distribution.log_prob(labels)
+    neg_log_likelihood = -tf.reduce_mean(input_tensor=log_likelihood)
+    kl = sum(model.losses) / len(x_train) * tf.minimum(1.0, kl_regularizer)
+
+    loss = neg_log_likelihood + kl
+        
+
+    # Build metrics for evaluation. Predictions are formed from a single forward
+    # pass of the probabilistic layers. They are cheap but noisy
+    # predictions.
+    predictions = tf.argmax(input=logits, axis=1)
+
+
+    #######################################################################
+    # Storing mean and standard deviations for the trained weights
+    #######################################################################
+    qmeans = []
+    qstds = []
+    names = []
+    # Actual_name = []
+    for i, layer in enumerate(model.layers):
+        try:
+            # print(layer)
+            q = layer.kernel_posterior
+        except AttributeError:
+            #print ("I am continuing layer has no attribute")
+            continue
+        names.append("Layer {}".format(i))
+        # Actual_name.append(layer)
+        qmeans.append(q.mean())
+        qstds.append(q.stddev())
+    
+    with tf.name_scope("train"):
+        train_accuracy, train_accuracy_update_op = tf.compat.v1.metrics.accuracy(labels=labels, predictions=predictions)
+
+        opt = tf.train.AdamOptimizer(FLAGS.learning_rate * hvd.size())
+        
+        # Horovod: add Horovod Distributed Optimizer.
+        opt = hvd.DistributedOptimizer(opt)
+       
+        global_step = tf.train.get_or_create_global_step()
+        
+        train_op = opt.minimize(loss, global_step=global_step)
+
+        update_step_op = tf.assign(t, t + 1)
+    
+    with tf.name_scope("valid"):
+        valid_accuracy, valid_accuracy_update_op = tf.metrics.accuracy(labels=labels, predictions=predictions)
+
+        correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(labels))
+        
+        Def_accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+
+    hooks = [
+            # Horovod: BroadcastGlobalVariablesHook broadcasts initial variable states
+            # from rank 0 to all other processes. This is necessary to ensure consistent
+            # initialization of all workers when training is started with random weights
+            # or restored from a checkpoint.
+            hvd.BroadcastGlobalVariablesHook(0),
+
+            # Horovod: adjust number of steps based on number of CPUs.
+            tf.train.StopAtStepHook(last_step= training_steps // hvd.size()),
+
+        ]
+
+    class Dummy():
+        pass
+
+    ## Creating some variable for runtime writing
+    net = Dummy()
+    net.plot = Dummy()
+    net.Totalruntimeworker = []
+    net.plot.RuntimeworkerIter = []
+    net.plot.Loss= []
+    net.plot.Accuracy= []
+    net.plot.Iter_num= []
+
+
+    # Horovod: pin GPU to be used to process local rank (one GPU per process)
+    config.gpu_options.allow_growth = True
+    config.gpu_options.visible_device_list = str(hvd.local_rank())
+
+    # Horovod: save checkpoints only on worker 0 to prevent other workers from
+    # corrupting them.
+    checkpoint_dir = os.path.join(FLAGS.model_dir,'train_logs') if hvd.rank() == 0 else None
+ 
+    if hvd.rank() == 0:
+        Results_file =  os.path.join(FLAGS.model_dir,'Timing_results_'+str(hvd.size())+'.txt')
+        Results_Train_file =  os.path.join(FLAGS.data_dir,'Results_Train_'+str(hvd.size())+'.csv')
+        
+        if os.path.exists(Results_file):
+            # append the file
+            f = open(Results_file,"a")
+            f_1 = open(Results_Train_file,"a")
+        else:
+            f = open(Results_file,"w",1)
+            f_1 = open(Results_Train_file,"w")
+            
+
+
+
+    stream_vars_valid = [ v for v in tf.local_variables() if "valid/" in v.name ]
+    reset_valid_op = tf.variables_initializer(stream_vars_valid)
+
+ 
+    training_batch_generator = DataObj.train_input_generator(x_train, y_train, batch_size=FLAGS.batch_size)
+    test_batch_generator = DataObj.train_input_generator(x_test, y_test, batch_size=FLAGS.batch_size)
+
+
+    with tf.train.MonitoredTrainingSession(checkpoint_dir=checkpoint_dir,hooks=hooks,config=config) as sess:
+
+        # Uncomment to see model Parameters & Graph
+        if FLAGS.verbose:
+            if hvd.rank() == 0:
+                print(model.summary(),flush=True)
+
+        # Run the training loop
+        # Time start
+        start_Train_time = time.time()
+        Time_to_train = 0
+        batch_process = 0
+
+        while not sess.should_stop():
+
+            Image , Label = next(training_batch_generator)
+            step = sess.run(global_step)
+    
+            start_batch_time = time.time()
+            
+            _ = sess.run([train_op,train_accuracy_update_op,update_step_op],feed_dict={images: Image,labels:Label})
+        
+            end_batch_time = time.time()
+
+            batch_process += 1
+            Time_per_batch = end_batch_time - start_batch_time
+            Time_to_train += Time_per_batch
+            
+
+            if hvd.rank() ==0:        
+                f.write(str(step) + "," + str(hvd.size()*float(FLAGS.batch_size)/Time_per_batch) + "," + str(Time_per_batch)+"," + str(start_batch_time)+","+str(end_batch_time)+ ","+ str(Time_to_train) +"\n")
+                f.flush()
+
+            
+
+            # Manually print the frequency
+            if step % FLAGS.print_step == 0 and (not sess.should_stop()) == True  :    
+                    
+                loss_value, accuracy_value, kl_value = sess.run([loss, train_accuracy, kl], feed_dict={images: Image,labels:Label})
+                    
+
+                net.plot.Iter_num.append(step)
+                net.plot.Loss.append(loss_value)
+                net.plot.Accuracy.append(accuracy_value)
+
+
+
+                Write = str(step) +','+ str(loss_value)+','+ str(accuracy_value)+','+ str(kl_value)+','+str(Time_to_train)+"\n"
+                out_print = "##Step: {:>3d} Loss: {:.3f} Avg_Accuracy: {:.3f} KL: {:.3f}  Time: {:.3f}".format(step, loss_value, accuracy_value, kl_value,Time_to_train)
+                
+                # Log_print(out_print,hvd.rank())
+
+                qm_vals, qs_vals = sess.run((qmeans, qstds))
+                
+                # Store the data for future plotting and visualization
+                #Log_print('Evalated qm_vals, qs_vals now storing...!',hvd.rank())
+                
+                with open(FLAGS.data_dir + "SavedWeight_Mean_STD_DATA_"+str(step), "wb") as out:
+                    pickle.dump([names,qm_vals, qs_vals], out)
+            
+                    if HAS_SEABORN:
+                        #print ('Evalated qm_vals, qs_vals now Plotting & storing...!',flush=True)
+                        fname = FLAGS.data_dir + "SavedPlot_Mean_STD"+str(step)+'.png'
+                        PrePostObj.plot_weight_posteriors(names, qm_vals, qs_vals, fname)
+
+                            
+                if hvd.rank() == 0:
+                    f_1.write(Write)
+                    f_1.flush()
+                    
+
+            
+            if hvd.rank() == 0:
+                
+                if (step + 1) % FLAGS.eval_freq == 0:                
+                    
+                    # train_handle, heldout_handle  = sess.run([training_iterator,heldout_iterator])
+                    Test_Image , Test_Label = next(test_batch_generator)
+                    
+                    # Compute log prob of heldout set by averaging draws from the model:
+                    # p(heldout | train) = int_model p(heldout|model) p(model|train)
+                    #                   ~= 1/n * sum_{i=1}^n p(heldout | model_i)
+                    # where model_i is a draw from the posterior
+                    # p(model|train).
+                    probs = np.asarray([sess.run((labels_distribution.probs),
+                                                feed_dict={images: Test_Image, labels:Test_Label})
+                                        for _ in range(FLAGS.num_monte_carlo)])
+                    mean_probs = np.mean(probs, axis=0)
+
+                    _, label_vals = sess.run(
+                        (images, labels), feed_dict={images: Test_Image, labels:Test_Label})
+                    
+                    heldout_lp = np.mean(np.log(mean_probs[np.arange(mean_probs.shape[0]),
+                                                        np.int32(label_vals).flatten()]))
+                    
+                    
+                    print(" #... Held-out nats: {:.3f}".format(heldout_lp))
+
+                    # # Calculate validation accuracy
+                    for _ in range(20):
+                        sess.run(valid_accuracy_update_op, feed_dict={images: Test_Image, labels:Test_Label})
+                    
+                    valid_value = sess.run(valid_accuracy, feed_dict={images: Test_Image, labels:Test_Label})
+
+                    
+                    print( (str(step)+','+str(heldout_lp)+','+str(valid_value)+"\n"), flush=True)
+                    
+
+                    sess.run(reset_valid_op)
+                # else:
+                #     Test_loss_value, Test_accuracy_value = sess.run([loss, Def_accuracy], feed_dict={images: Test_Image,labels:Test_Label})
+                #     f_2.write(str(step)+","+str(Test_loss_value)+","+ str(Test_accuracy_value)+"\n")
+                #     f_2.flush()
+                #     #out_print = "##Step: {:>3d} Loss: {:.3f} Avg_Accuracy: {:.3f} Time: {:.3f}".format(step, loss_value, Test_accuracy_value)
+
+
+        end_Train_time = time.time()
+        diff_trainSess = end_Train_time-start_Train_time
+
+
+        # Each Rank dumps results for Accu         
+        with open( os.path.join(FLAGS.data_dir , ("PlotRunTimeIteration_Rank_" + str(hvd.rank())) ), "wb") as out:
+            pickle.dump([net.plot.Iter_num,net.plot.Loss,net.plot.Accuracy], out)
+     
+        if hvd.rank()==0:
+            f.close()
+            f_1.close()
+
+
+
+if __name__ == "__main__":
+    tf.app.run()
+    
--- src/utils.py
+++ src/utils.py
@@ -9,13 +9,28 @@ from tensorflow import keras
 import pickle
 import tensorflow_probability as tfp
 import sys
+from tensorflow.python.keras import backend as K
+from tensorflow.python.keras.datasets.cifar import load_batch
 from absl import flags
 
+
+
+try:
+  import seaborn as sns 
+  HAS_SEABORN = True
+except ImportError:
+  HAS_SEABORN = False
+  
 tfp_layers = tfp.layers
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
 tfd = tfp.distributions
+
+from matplotlib import figure 
+from matplotlib.backends import backend_agg
+
+matplotlib.use("Agg")
 
 
 
@@ -25,6 +40,17 @@ class Pre_Post_Process:
         super().__init__()
         self.FLAGS = FLAGS
     
+    def Setup_Seed(self,rank):
+        seed_adjustment= rank
+        tf.reset_default_graph()
+        np.random.seed(6118 + seed_adjustment)
+        tf.set_random_seed(1234 + seed_adjustment)
+        original_seed = 1092 + seed_adjustment
+        seed = tfd.SeedStream(original_seed, salt="random_beta")
+        
+        return seed
+
+
     def create_config_proto(self):
         config = tf.ConfigProto()
         config.intra_op_parallelism_threads = self.FLAGS.num_intra
@@ -57,6 +83,118 @@ class Pre_Post_Process:
                         this_variable_parameters
                     ))
                     _out.write( "Total trainable parameters for this network: {} \n".format(total_parameters))
+    
+    def Log_print(self,statement,rank):
+        ##
+        # The print by rank-0 only
+        ##
+        assert isinstance(statement,str),"Statement need to be a String datatype..!"
+        if rank == 0:
+            str_ = "[Rank-{}] ".format(rank)
+            print (str_ + statement,flush=True)
+        else:
+            pass
+        return
+    
+    def plot_weight_posteriors(self,names, qm_vals, qs_vals, fname):
+
+        fig = figure.Figure(figsize=(24, 12))
+        canvas = backend_agg.FigureCanvasAgg(fig)
+        
+        ax = fig.add_subplot(1, 2, 1)
+
+        for n, qm in zip(names, qm_vals):
+            sns.distplot(qm.flatten(), ax=ax, label=n)
+        ax.set_title("weight means")
+        #ax.set_xlim([-1.5, 1.5])
+        ax.legend()
+        
+        ax = fig.add_subplot(1, 2, 2)
+        for n, qs in zip(names, qs_vals):
+            sns.distplot(qs.flatten(), ax=ax)
+        ax.set_title("weight stddevs")
+        #ax.set_xlim([0, 1.])
+        
+        fig.tight_layout()
+        canvas.print_figure(fname, format="png")
+        #print("saved {}".format(fname))
+        return
+
+
+class Data_API:
+    def __init__(self,FLAGS):
+        super().__init__()
+        self.FLAGS = FLAGS
+
+    
+    def load_data(self, rank):
+        """Loads CIFAR10 dataset.
+        Returns:
+            Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
+        """
+        #path = '/projects/datascience/hsharma/bnn_horovod/TFP_CIFAR10/RunScript/cifar-10-batches-py'
+        #path = '/home/hsharma/WORK/Project_BNN/bnn_horovod/TFP_CIFAR10/cifar-10-batches-py'
+        
+        if self.FLAGS.DATA_NAME == 'CIFAR-10':
+            path = self.FLAGS.DATA_PATH
+            num_train_samples = 50000
+
+            x_train = np.empty((num_train_samples, 3, 32, 32), dtype='uint8')
+            y_train = np.empty((num_train_samples,), dtype='uint8')
+
+
+            for i in range(1, 6):
+                fpath = os.path.join(path, 'data_batch_' + str(i))
+                (x_train[(i - 1) * 10000:i * 10000, :, :, :],
+                y_train[(i - 1) * 10000:i * 10000]) = load_batch(fpath)
+
+            fpath = os.path.join(path, 'test_batch')
+            x_test, y_test = load_batch(fpath)
+
+            y_train = np.reshape(y_train, (len(y_train), 1))
+            y_test = np.reshape(y_test, (len(y_test), 1))
+
+            if K.image_data_format() == 'channels_last':
+                x_train = x_train.transpose(0, 2, 3, 1)
+                x_test = x_test.transpose(0, 2, 3, 1)
+
+            x_test = x_test.astype(x_train.dtype)
+            y_test = y_test.astype(y_train.dtype)
+
+            x_train = x_train.astype("float32")
+            x_test = x_test.astype("float32")
+
+            x_train /= 255
+            x_test /= 255
+
+            if FLAGS.subtract_pixel_mean:
+                x_train_mean = np.mean(x_train, axis=0)
+                x_train -= x_train_mean
+                x_test -= x_train_mean
+                
+            # y_train = y_train.flatten()
+            # y_test = y_test.flatten()
+            y_train= np.int32(y_train)
+            y_test= np.int32(y_test)
+        
+        else:
+            print("ERROR: The dataset is not Available...!")
+            return
+        return (x_train, y_train), (x_test, y_test)
+
+    def train_input_generator(self, x_train, y_train, batch_size=64):
+        assert len(x_train) == len(y_train)
+        while True:
+            p = np.random.permutation(len(x_train))
+            x_train, y_train = x_train[p], y_train[p]
+            index = 0
+            while index <= len(x_train) - batch_size:
+                yield x_train[index:index + batch_size], np.reshape(y_train[index:index + batch_size], -1),
+                index += batch_size
+
+
+
+
 
 
 class Model_CNN_BNN:
@@ -143,5 +281,21 @@ class Model_CNN_BNN:
             (tf.keras.layers.Dense(self.Num_class))])
         return model
 
-
-
+    def CIFAR10_BNN_model(self):
+        """
+        CIFAR-10 Basic Model..
+        """
+        model = tf.keras.Sequential([
+            (tfp_layers.Convolution2DFlipout(32,kernel_size=[3,3],activation=tf.nn.relu,padding='SAME',input_shape=self.feature_shape,name='Conv_I')),
+            (tfp_layers.Convolution2DFlipout(32,kernel_size=[3,3],activation=tf.nn.relu,padding='SAME',name='Conv_II')),
+            (tf.keras.layers.MaxPooling2D(pool_size=(2, 2), padding='same', strides=2,name='Max_I_')),
+            (tfp_layers.Convolution2DFlipout(64,kernel_size=[3,3],activation=tf.nn.relu,padding='SAME',name='Conv_III')),
+            (tfp_layers.Convolution2DFlipout(64,kernel_size=[3,3],activation=tf.nn.relu,padding='SAME',name='Conv_IV')),
+            (tf.keras.layers.MaxPooling2D(pool_size=(2, 2), padding='same', strides=2,name='Max_II')),
+            (tfp_layers.Convolution2DFlipout(128,kernel_size=[3,3],activation=tf.nn.relu,padding='SAME',name='Conv_V')),
+            (tfp_layers.Convolution2DFlipout(128,kernel_size=[3,3],activation=tf.nn.relu,padding='SAME',name='Conv_VI')),
+            (tf.keras.layers.MaxPooling2D(pool_size=(2, 2), padding='same', strides=2,name='Max_III')),
+            (tf.keras.layers.Flatten()),
+            (tfp_layers.DenseFlipout(128,activation=tf.nn.relu,padding='SAME',name='Dense_I')),
+            (tfp_layers.DenseFlipout(10,padding='SAME',name='Dense_II')),
+            ])
